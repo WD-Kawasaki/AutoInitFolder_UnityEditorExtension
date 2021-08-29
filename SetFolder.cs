1d65ebe16f4303c88343ee06c6d67116fa781f23
WDEXcourse <86914835+WDEXcourse@users.noreply.github.com>
2021-08-29 22:15:47 +0900

    自動でフォルダ生成するscripte

---
+++ SetFolder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class SetFolder
+{
+    [MenuItem("Custom/CreateFolder")]
+    static void CreateFolder()
+    {
+        if(AssetDatabase.IsValidFolder("Assets/"))
+        {
+            return;
+        }
+        FloderName("Animations");
+        FloderName("AnimatorControllers");
+        FloderName("Textures");
+        FloderName("Materials");
+        FloderName("Prefabs");
+        FloderName("Scripts");
+    }
+
+    static void FloderName(string name)
+    {
+        string path = "Assets/" + name;
+
+        if(!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        AssetDatabase.ImportAsset(path);
+    }
+
+}
